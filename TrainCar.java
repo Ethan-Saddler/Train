23c16461a9c58289135b30172c9cd5b77b73a4fd
Ethan <ethansaddlerm@gmail.com>
2022-11-28 23:05:32 -0500

    traincar

--- TrainCar.java
+++ TrainCar.java
@@ -1,1 +1,30 @@
+public class TrainCar<T> {
+    private T cargo;
+    private T nextCar;
 
+    public TrainCar(T cargo) {
+        if(cargo == null) {
+            throw new IllegalArgumentException();
+        }
+        this.cargo = cargo;
+        this.nextCar = null;
+    }
+
+    public TrainCar(T cargo, T nextCar) {
+        this(cargo);
+        this.nextCar = nextCar;
+    }
+
+    public T getCargo() {
+        return this.cargo;
+    }
+    public T getCar() {
+        return this.nextCar;
+    }
+    public void setCargo(T cargo) {
+        this.cargo = cargo;
+    }
+    public void setCar(T nextCar) {
+        this.nextCar = nextCar;
+    }
+}
